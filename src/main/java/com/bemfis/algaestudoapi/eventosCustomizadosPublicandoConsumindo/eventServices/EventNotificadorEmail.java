f8bf8838e7ea8a36371c07e9949cd0c6262a66e8
andrebemfica <andrecbemfica@gmail.com>
2023-03-31 11:54:19 -0300

    criada a classe EventProperties para acessar as propriedades usando @ConfigurationProperties e adicionada a dependência para autocomplete.

--- src/main/java/com/bemfis/algaestudoapi/eventosCustomizadosPublicandoConsumindo/eventServices/EventNotificadorEmail.java
+++ src/main/java/com/bemfis/algaestudoapi/eventosCustomizadosPublicandoConsumindo/eventServices/EventNotificadorEmail.java
@@ -1,24 +1,25 @@
 package com.bemfis.algaestudoapi.eventosCustomizadosPublicandoConsumindo.eventServices;
 
 import com.bemfis.algaestudoapi.eventosCustomizadosPublicandoConsumindo.eventModels.EventCliente;
+import com.bemfis.algaestudoapi.eventosCustomizadosPublicandoConsumindo.eventProperties.EventProperties;
 import com.bemfis.algaestudoapi.eventosCustomizadosPublicandoConsumindo.eventServices.anotacoes.EventTipoNotificador;
 import com.bemfis.algaestudoapi.eventosCustomizadosPublicandoConsumindo.eventServices.enuns.EventNivelUrgencia;
 import com.bemfis.algaestudoapi.eventosCustomizadosPublicandoConsumindo.eventServices.interfaces.EventNotificador;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @EventTipoNotificador(EventNivelUrgencia.URGENTE)
 @Component
 public class EventNotificadorEmail implements EventNotificador {
-    @Value("${notificador.email.host-servidor}")
-    private String host;
-    @Value("${notidicador.email.porta-servidor}")
-    private Integer porta;
+    @Autowired
+    private EventProperties properties;
+
     public void notificar(EventCliente eventCliente, String mensagem) {
         System.out.printf("Notificando %s através do e-mail %s: %s",
                 eventCliente.getNome(), eventCliente.getEmail(), mensagem);
 
-        System.out.println("Host: " + host);
-        System.out.println("Porta: " + porta);
+        System.out.println("Host: " + properties.getHostServidor());
+        System.out.println("Porta: " + properties.getPortaServidor());
     }
 }
